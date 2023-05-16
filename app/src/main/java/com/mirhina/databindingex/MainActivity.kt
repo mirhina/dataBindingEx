7e3a472faa9d218f708b31507c6b0b9833fe096e
mirhina <wjstkdgns96@naver.com>
2023-05-17 08:29:15 +0900

    강의 2 Commit

--- app/src/main/java/com/mirhina/databindingex/MainActivity.kt
+++ app/src/main/java/com/mirhina/databindingex/MainActivity.kt
@@ -2,6 +2,7 @@ package com.mirhina.databindingex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
@@ -30,5 +31,18 @@ class MainActivity : AppCompatActivity() {
         binding.testBtnId.setOnClickListener {
             Toast.makeText(this,"Click1",Toast.LENGTH_SHORT).show()
         }
+
+        var value = "this is value"
+        val value2 = "this is value2"
+
+        value = "this is not value" // val은 값 수정이 불가능하다. var로 변경
+
+        Log.d("MainActivity","Hello Log") // MainActivity에 로그 찍기 (하단 Logcat에서 확인 가능)
+        Log.w("MainActivity",value) // value 값 log에 경고 로그 표시
+        Log.i("MainActivity",value) // value 값 log에 정보 로그 표시
+        Log.e("MainActivity",value) // value 값 log에 오류 로그 표시
+        Log.v("MainActivity",value) // value 값 log에 상세 로그 표시
+        Log.d("MainActivity",value) // value 값 log에 디버그 로그 표시
+
     }
 }
